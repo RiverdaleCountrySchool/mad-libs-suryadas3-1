7d2da6706454e3347731a9bfa7061d10f7c2cb91
suryadas3 <sdas28@riverdale.edu>
2025-01-28 11:52:28 -0500

    added to code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println ("One day, I was walking my" + args [0] + "," + args [1] + "when all of a sudden I saw a big" + args [2] + "out of nowhere!")
-        System.out.println("My" + args [3] + "started" + args[4] + "at this monstrosity and I              . " + args[0] + " " + args [1] + "!");
+        System.out.println ("One day, I was walking my " + args [0] + ", " + args [1] + ", when all of a sudden I saw a big " + args [2] + " out of nowhere!");
+        System.out.println("My " + args [0] + " started " + args[3] + " at this monstrosity and I " + args[4] + ".");
+        System.out.println("We " + args [5] + " all the way back to our " + args [6] + ".");
+        System.out.println("We made " + args [7] + " for dinner and decided not to go out for a walk for the next " + args [8] + " " + args [9] + ".");
 
-
-        System.out.println("Hello, " + args[0] + " " + args [1] + "!");
     }
 }
