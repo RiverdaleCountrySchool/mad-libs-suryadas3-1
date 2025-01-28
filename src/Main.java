5b1b866366a0365e50c699da53f277a663317d57
suryadas3 <sdas28@riverdale.edu>
2025-01-28 11:38:48 -0500

    added to code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println ("One day, I was walking my" + args [0] + "," + args [1] + "when all of a sudden I saw a big" + args [2] + "out of nowhere!")
+        System.out.println("My" + args [3] + "started" + args[4] + "at this monstrosity and I              . " + args[0] + " " + args [1] + "!");
+
+
+        System.out.println("Hello, " + args[0] + " " + args [1] + "!");
     }
 }
